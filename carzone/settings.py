da409e22b33323730c86b79a2c8d6fcc85677b85
Antonio Marino <marino.ant@yahoo.com>
2022-12-05 15:14:30 +0100

    Added app Cars and fetched data into

--- carzone/settings.py
+++ carzone/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cars.apps.CarsConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
+    'multiselectfield',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -114,6 +118,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_L10N = True
+
+USE_TZ = True
 
 
 
--- pages/urls.py
+++ pages/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('services', views.services, name='services'),
     path('contact', views.contact, name='contact'),
     path('search', views.search, name='search'),
-    
+
+
     ]
