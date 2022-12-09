58d7b45807605dd11e90489ddce401f57ab25f4b
Antonio Marino <marino.ant@yahoo.com>
2022-12-09 13:54:02 +0100

    complete search funcionalities and admin customization

--- carzone/urls.py
+++ carzone/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf.urls.static import static
 from django.conf import settings
- 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('cars/', include('cars.urls')),
-] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+ ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
